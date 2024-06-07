e9a939afaf14b05b8b7c79369c289d5854a83805
Charles <cchester1@angelo.edu>
2024-06-07 11:45:06 -0500

    monitor recording disk usage

--- src/utils/monitor.py
+++ src/utils/monitor.py
@@ -10,17 +10,34 @@ def get_cpu_usage():
 	usage = psutil.cpu_percent(interval=1)
 	return usage
 
+def get_disk_usage():
+	space = psutil.disk_usage('/')
+	return {
+		"total": space.total,
+		"used": space.used,
+		"free": space.free,
+		"percent": space.percent
+      }
+
 def log_cpu_usage(file_path):
 	while True:
 		try:
 			cpu_usage = get_cpu_usage()
+			disk_usage = get_disk_usage()
 			timestamp = time.time()
-			data = {"timestamp": timestamp, "cpu_usage":cpu_usage}
-
+			data = {
+				      "timestamp": timestamp, 
+	                        "cpu_usage": cpu_usage, 
+					"disk_usage": disk_usage
+				}
+			
+                  # write to the logs
 			with open(file_path, "a") as file:
 				file.write(json.dumps(data) + "\n")
 
+                  # Sent to runMonitor stdout
 			print(f"Usage: {cpu_usage}")
+			print(f"Space: {disk_usage}")
 
 			time.sleep(10) # Run every 10 seconds
 		except Exception as e:
